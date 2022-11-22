a1023c603508aa1e8a597baa48a669e4b6a96f08
ImedZeiri <imedzairi1996@gmail.com>
2022-11-22 21:56:37 +0100

    Retouche_Finale_Code_Done

--- JobFinder/JobFinder/urls.py
+++ JobFinder/JobFinder/urls.py
@@ -1,7 +1,7 @@
 """JobFinder URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,11 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views
 
-from core.views import frontpage
+from core.views import frontpage, signup
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path('', frontpage, name='frontpage'),
+    path('signup/', signup, name='signup'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('admin/', admin.site.urls),
+    path('dashboard/', include('userprofile.urls')),
+    path('notifications/', include('notification.urls')),
+    path('jobs/', include('job.urls')),
 ]
